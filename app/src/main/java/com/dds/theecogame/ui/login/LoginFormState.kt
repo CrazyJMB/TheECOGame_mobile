81cde39f8dd9e7352b662f57a6a261f6f7a50b9c
CrazyJMB <crazyjmbemail@gmail.com>
2023-03-11 19:50:28 +0100

    Commit inicial

---
+++ app/src/main/java/com/dds/theecogame/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.dds.theecogame.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
