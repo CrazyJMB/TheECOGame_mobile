34ff1b09f94b4c3c0ff0b38d99e4f6312b167ea8
Youcef Kechouane Benfekhadou <ykechou@epsa.upv.es>
2023-03-12 21:16:47 +0100

    [UT 385] Interfaz de Preguntas, realizado por Edwin y Youcef

---
+++ app/src/main/java/com/dds/theecogame/viewmodel/Activity_questions.kt
@@ -0,0 +1,12 @@
+package com.dds.theecogame.viewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dds.theecogame.R
+
+class activity_questions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
