86a70776771f79bd36f79b1e478ff2de94d44fac
Youcef Kechouane Benfekhadou <ykechou@epsa.upv.es>
2023-03-19 19:27:32 +0100

    [UT 385] Fragmentos de Preguntas

---
+++ app/src/main/java/com/dds/theecogame/ui/Questions.kt
@@ -0,0 +1,12 @@
+package com.dds.theecogame.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dds.theecogame.R
+
+class Questions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
