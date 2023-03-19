4f6606c4c8193be802ec8c001ad9787c01786bec
Edwin Makoveev Routskaia <makoveevedwin@gmail.com>
2023-03-19 19:45:13 +0100

    [UT-361] Solucionado el error que me daba, NO QUITAR EL view: View

--- app/src/main/java/com/dds/theecogame/activity_estadistica.kt
+++ app/src/main/java/com/dds/theecogame/activity_estadistica.kt
@@ -3,6 +3,7 @@ package com.dds.theecogame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.dds.theecogame.databinding.ActivityEstadisticaBinding
 import com.dds.theecogame.ui.MainScreen
 
@@ -16,20 +17,20 @@ class activity_estadistica : AppCompatActivity() {
         initializeStats()
     }
 
-    fun goToMainScreen (){
+    fun goToMainScreen (view: View){
         val mainScreen = Intent(this, MainScreen::class.java)
         startActivity(mainScreen)
     }
 
     private fun initializeStats (){
-        binding.numPartidasJugadas.setText(binding.numPartidasJugadas.text.toString() + " 0")
-        binding.numPartidasGanadas.setText(binding.numPartidasGanadas.text.toString() + " 0")
-        binding.numPartidasPerdidas.setText(binding.numPartidasPerdidas.text.toString() + " 0")
-        binding.numPartidasAbandonadas.setText(binding.numPartidasAbandonadas.text.toString() + " 0")
-        binding.tiempoPromedio.setText(binding.tiempoPromedio.text.toString() + " 0")
-        binding.tiempoTotal.setText(binding.tiempoTotal.text.toString() + " 0")
-        binding.numPreguntasAcertadas.setText(binding.numPreguntasAcertadas.text.toString() + " 0")
-        binding.numPreguntasIncorrectas.setText(binding.numPreguntasIncorrectas.text.toString() + " 0")
-        binding.nivelConocimientoODS.setText(binding.nivelConocimientoODS.text.toString() + " 0%")
+        binding.numPartidasJugadas.text = binding.numPartidasJugadas.text.toString() + " 0"
+        binding.numPartidasGanadas.text = binding.numPartidasGanadas.text.toString() + " 0"
+        binding.numPartidasPerdidas.text = binding.numPartidasPerdidas.text.toString() + " 0"
+        binding.numPartidasAbandonadas.text = binding.numPartidasAbandonadas.text.toString() + " 0"
+        binding.tiempoPromedio.text = binding.tiempoPromedio.text.toString() + " 0"
+        binding.tiempoTotal.text = binding.tiempoTotal.text.toString() + " 0"
+        binding.numPreguntasAcertadas.text = binding.numPreguntasAcertadas.text.toString() + " 0"
+        binding.numPreguntasIncorrectas.text = binding.numPreguntasIncorrectas.text.toString() + " 0"
+        binding.nivelConocimientoODS.text = binding.nivelConocimientoODS.text.toString() + " 0%"
     }
 }
