636e29cf158d0d378166ad43dfc28f3c31a75618
Juan Francisco López Quilis <JFLOPQUI@upv.edu.es>
2023-03-27 16:20:55 +0200

    Pruebas uso fragmentos

--- app/src/main/java/com/dds/theecogame/presentation/views/GameActivityView.kt
+++ app/src/main/java/com/dds/theecogame/presentation/views/GameActivityView.kt
@@ -1,12 +1,12 @@
-package com.dds.theecogame.ui
+package com.dds.theecogame.presentation.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dds.theecogame.R
 
-class Consolidate : AppCompatActivity() {
+class GameActivityView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_consolidate)
+        setContentView(R.layout.activity_game)
     }
 }
--- app/src/main/java/com/dds/theecogame/presentation/views/MainScreenActivityView.kt
+++ app/src/main/java/com/dds/theecogame/presentation/views/MainScreenActivityView.kt
@@ -1,12 +1,12 @@
-package com.dds.theecogame.views
+package com.dds.theecogame.presentation.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.dds.theecogame.R
-import com.dds.theecogame.viewmodel.MainScreenViewModel
+import com.dds.theecogame.presentation.viewmodel.MainScreenViewModel
 
-class MainScreen : AppCompatActivity() {
+class MainScreenActivityView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
