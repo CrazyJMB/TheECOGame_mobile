40909dc50ea233260d0c4377cba00dc2a5bb2c5f
Edwin Makoveev Routskaia <makoveevedwin@gmail.com>
2023-04-02 18:31:44 +0200

    fragmento resumen a medio terminar

--- app/src/main/java/com/dds/theecogame/presentation/viewmodel/MainScreenViewModel.kt
+++ app/src/main/java/com/dds/theecogame/presentation/viewmodel/MainScreenViewModel.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.view.View
 import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModel
-import com.dds.theecogame.activity_estadistica
-import com.dds.theecogame.views.MainScreen
 
 class MainScreenViewModel : ViewModel() {
     // Logica compleja o datos que deban actualizarle en la vista
