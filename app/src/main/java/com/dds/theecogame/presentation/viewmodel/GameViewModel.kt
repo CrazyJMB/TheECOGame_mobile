3311d1c118015834ba102db5b717f71f20025116
Edwin Makoveev Routskaia <makoveevedwin@gmail.com>
2023-04-02 21:50:02 +0200

    Avance en el fragmento resumen

--- app/src/main/java/com/dds/theecogame/presentation/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/dds/theecogame/presentation/viewmodel/GameViewModel.kt
@@ -3,5 +3,19 @@ package com.dds.theecogame.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
-    fun saveResults()
+    internal fun sendResults() {
+        //Funcionalidad a implementar mas tarde cuando haya participantes, y ahi guardamos sus stats
+    }
+
+    internal fun getResults(): List<Int> {
+        //Conseguir Tiempo Jugado en la Partida, Puntos Obtenidos y Num Preguntas respondidas
+        //He pensado que directamente que haya un fichero .txt donde hay pongamos eso
+        //Y a partir de ese fichero obtener esos datos
+        var summaryStats = mutableListOf<Int>()
+        /*IMPLEMENTAR*/
+        return summaryStats
+    }
+
+
+
 }
--- app/src/main/java/com/dds/theecogame/presentation/views/FragmentResumen.kt
+++ app/src/main/java/com/dds/theecogame/presentation/views/FragmentResumen.kt
@@ -6,46 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dds.theecogame.R
 import com.dds.theecogame.databinding.FragmentResumenBinding
-import com.dds.theecogame.presentation.viewmodel.MainScreenViewModel
+import com.dds.theecogame.presentation.viewmodel.GameViewModel
 import com.dds.theecogame.presentation.views.MainScreenActivityView as MainScreenActivityView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class FragmentResumen : Fragment() {
-    private var _binding: FragmentResumenBinding? = null
-    private val binding get()= _binding!!
+    private lateinit var binding: FragmentResumenBinding
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    //Codigo de inicializacion (como configurar una variable)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-
-        }
+        initializeSummary()
     }
 
+    //Inflar el diseño del fragment y devolver la vista
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentResumenBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        binding = FragmentResumenBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    //Codigo para inicializar la vista (como podria ser un listener para un boton)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.acabarPartida.setOnClickListener {
             //Enviar los datos para que se guarden y tal...
+            GameViewModel().sendResults()
 
 
             //Irse al menu principal
@@ -55,23 +43,10 @@ class FragmentResumen : Fragment() {
         }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentResumen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentResumen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initializeSummary (){
+        val statValues = GameViewModel().getResults()
+        binding.timePlayed.text = "${binding.timePlayed.text} ${statValues[0]}"
+        binding.pointsObtained.text = "${binding.pointsObtained.text} ${statValues[1]}"
+        binding.questionsAnswered.text = "${binding.questionsAnswered.text} ${statValues[2]}"
     }
 }
