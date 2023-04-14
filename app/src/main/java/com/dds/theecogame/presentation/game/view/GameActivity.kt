d3cc158068d20198b5d38e4a64e3d3bfe912dcc2
Edwin Makoveev Routskaia <makoveevedwin@gmail.com>
2023-04-14 19:45:16 +0200

    Muchos cambios de Edwin y Youcef

--- app/src/main/java/com/dds/theecogame/presentation/game/view/GameActivity.kt
+++ app/src/main/java/com/dds/theecogame/presentation/game/view/GameActivity.kt
@@ -1,9 +1,14 @@
 package com.dds.theecogame.presentation.game.view
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.dds.theecogame.R
 import com.dds.theecogame.databinding.ActivityGameBinding
+import com.dds.theecogame.presentation.game.viewModel.GameViewModel
 import com.dds.theecogame.presentation.mainScreen.view.MainScreenActivity
 
 class GameActivity : AppCompatActivity() {
@@ -11,13 +16,28 @@ class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initialize()
+
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.Volver.setOnClickListener {
-            val intent = Intent(this, MainScreenActivity::class.java)
-            startActivity(intent)
-            TODO("NO se puede salir si no se ha consolidado -> Mensaje")
+            val sharedPref = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("¿Deseas abandonar?")
+            builder.setMessage("Recuerda que si no has consolidado, obtendras 0 puntos")
+            builder.setPositiveButton("Aceptar") { dialog, which ->
+                GameViewModel().changeGameStatus(sharedPref,"Abandoned")
+                val summaryFragment = ResumeFragment()
+                val fragmentManager = this.supportFragmentManager
+                fragmentManager.beginTransaction()
+                    .replace(R.id.GameContainerView, summaryFragment)
+                    .commit()
+            }
+            builder.setNegativeButton("Cancelar") { dialog, which ->
+                //No hace nada
+            }
+            builder.show()
         }
     }
 
@@ -26,5 +46,9 @@ class GameActivity : AppCompatActivity() {
         TODO("Que pasa cuando se sale de una partida no acabada")
     }
 
+    private fun initialize() {
+        val sharedPref = getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        GameViewModel().resetGameStats(sharedPref)
+    }
 
 }
--- app/src/main/java/com/dds/theecogame/presentation/game/view/ResumeFragment.kt
+++ app/src/main/java/com/dds/theecogame/presentation/game/view/ResumeFragment.kt
@@ -1,5 +1,6 @@
 package com.dds.theecogame.presentation.game.view
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,12 +15,6 @@ import com.dds.theecogame.presentation.mainScreen.view.MainScreenActivity
 class ResumeFragment : Fragment() {
     private lateinit var binding: FragmentResumenBinding
 
-    //Codigo de inicializacion (como configurar una variable)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //initializeSummary()
-    }
-
     //Inflar el diseño del fragment y devolver la vista
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +27,8 @@ class ResumeFragment : Fragment() {
     //Codigo para inicializar la vista (como podria ser un listener para un boton)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initializeSummary()
+
         binding.acabarPartida.setOnClickListener {
             //Enviar los datos para que se guarden y tal...
             GameViewModel().sendResults()
@@ -39,20 +36,25 @@ class ResumeFragment : Fragment() {
             //Irse al menu principal
             val mainScreen = Intent(activity, MainScreenActivity::class.java)
             startActivity(mainScreen)
-            activity?.finish()
         }
     }
 
     private fun initializeSummary() {
-        val statValues = GameViewModel().getResults()
-        binding.timePlayed.text = "${binding.timePlayed.text} ${statValues[0]}"
+        val sharedPref = requireActivity().getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+        GameViewModel().setTimeEnded(sharedPref)
+        val statValues = GameViewModel().getResults(sharedPref)
+
+        val minutes = statValues[0]/60
+        val seconds = statValues[0]
+
+        binding.timePlayed.text = "${binding.timePlayed.text} ${minutes}min ${seconds}sec"
         binding.pointsObtained.text = "${binding.pointsObtained.text} ${statValues[1]}"
         binding.questionsAnswered.text = "${binding.questionsAnswered.text} ${statValues[2]}/10"
 
-        if (GameViewModel().hasUserAbandoned()) {
+        if (GameViewModel().hasUserAbandoned(sharedPref)) {
             binding.Title.setText(R.string.abandoned)
         } else {
-            if (GameViewModel().hasUserWon()) {
+            if (GameViewModel().hasUserWon(sharedPref)) {
                 binding.Title.setText(R.string.victory)
             } else {
                 binding.Title.setText(R.string.lost)
