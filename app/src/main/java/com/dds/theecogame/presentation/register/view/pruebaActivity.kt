72226aa8ac5610cf1c8ab8dbe7624c33af5f8cdf
Juan Francisco López Quilis <JFLOPQUI@upv.edu.es>
2023-04-28 19:43:43 +0200

    [UT-363] Vista Registro

---
+++ app/src/main/java/com/dds/theecogame/presentation/register/view/pruebaActivity.kt
@@ -0,0 +1,12 @@
+package com.dds.theecogame.presentation.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dds.theecogame.R
+
+class pruebaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prueba)
+    }
+}
