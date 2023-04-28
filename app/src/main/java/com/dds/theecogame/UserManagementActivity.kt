a29f4e31089070562309366a35eaf1b0b1e21d58
Juan Francisco López Quilis <JFLOPQUI@upv.edu.es>
2023-04-28 12:18:50 +0200

    [UT-1497] Estructura vista Inicio (modificación)

---
+++ app/src/main/java/com/dds/theecogame/UserManagementActivity.kt
@@ -0,0 +1,11 @@
+package com.dds.theecogame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_management)
+    }
+}
