7306820777283269af85d9045214f7973297d4e7
Juan Francisco López Quilis <JFLOPQUI@upv.edu.es>
2023-05-02 19:26:02 +0200

    [UT-363] Lógica Registro

--- app/src/main/java/com/dds/theecogame/presentation/register/view/RegisterActivity.kt
+++ app/src/main/java/com/dds/theecogame/presentation/register/view/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.dds.theecogame.presentation.register.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -24,27 +25,36 @@ class RegisterActivity : AppCompatActivity() {
 
         binding.etUsername.setOnFocusChangeListener { view, b ->
             if (!b) {
-                var error = viewModel.checkUsername(binding.etUsername.text)
+                var error = viewModel.checkUsername(binding.etUsername.text.toString())
                 if (!error.isNullOrEmpty()) {
+                    binding.tvUsernameError.visibility = View.VISIBLE
                     binding.tvUsernameError.text = error
+                } else {
+                    binding.tvUsernameError.visibility = View.INVISIBLE
                 }
             }
         }
 
         binding.etEmail.setOnFocusChangeListener { view, b ->
             if (!b) {
-                var error = viewModel.checkUsername(binding.etUsername.text)
+                var error = viewModel.checkEmail(binding.etEmail.text.toString())
                 if (!error.isNullOrEmpty()) {
+                    binding.tvEmailError.visibility = View.VISIBLE
                     binding.tvEmailError.text = error
+                } else {
+                    binding.tvEmailError.visibility = View.INVISIBLE
                 }
             }
         }
 
         binding.etPassword.setOnFocusChangeListener { view, b ->
             if (!b) {
-                var error = viewModel.checkUsername(binding.etUsername.text)
+                var error = viewModel.checkPassword(binding.etPassword.text.toString())
                 if (!error.isNullOrEmpty()) {
+                    binding.tvPasswordError.visibility = View.VISIBLE
                     binding.tvPasswordError.text = error
+                } else {
+                    binding.tvPasswordError.visibility = View.INVISIBLE
                 }
             }
         }
--- app/src/main/java/com/dds/theecogame/presentation/register/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/dds/theecogame/presentation/register/viewModel/RegisterViewModel.kt
@@ -4,8 +4,16 @@ import androidx.lifecycle.ViewModel
 import com.dds.theecogame.presentation.game.viewModel.GameViewModel
 
 class RegisterViewModel : ViewModel() {
-    fun checkUsername(text: Editable?): CharSequence? {
+    fun checkUsername(username: String): String {
+        return username
+    }
 
+    fun checkEmail(email: String): CharSequence? {
+        return email
+    }
+
+    fun checkPassword(password: String): CharSequence? {
+        return password
     }
 
 }
