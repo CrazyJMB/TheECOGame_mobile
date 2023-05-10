f0e9d589b5716333b7d290744d78da5de1dbf27f
Juan Francisco López Quilis <JFLOPQUI@upv.edu.es>
2023-05-10 19:09:48 +0200

    Estructura Factory

---
+++ app/src/main/java/com/dds/theecogame/domain/factory/validators/UsernameValidator.kt
@@ -0,0 +1,31 @@
+package com.dds.theecogame.domain.factory.validators
+
+import com.dds.theecogame.domain.factory.Validator
+import com.dds.theecogame.domain.repository.UserRepository
+
+class UsernameValidator : Validator {
+    override var error: String
+        get(): String {
+            return this.error
+        }
+        set(value) {
+            this.error = value
+        }
+
+    override var check: Boolean
+        get(): Boolean {
+            return this.check
+        }
+        set(value) {
+            this.check = value
+        }
+
+    override val userRepository: UserRepository
+        get(): UserRepository {
+            return this.userRepository
+        }
+
+    override fun validate(username: String): Boolean {
+        return true
+    }
+}
