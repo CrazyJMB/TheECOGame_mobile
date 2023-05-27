ef6a3e9c2e11f7bb67cba606c364faf7dfcedec3
Juan Francisco López Quilis <JFLOPQUI@upv.edu.es>
2023-05-27 17:51:31 +0200

    UT-357. Icono ranking

--- app/src/main/java/com/dds/theecogame/data/remote/statistics/dto/RankingDto.kt
+++ app/src/main/java/com/dds/theecogame/data/remote/statistics/dto/RankingDto.kt
@@ -3,13 +3,13 @@ package com.dds.theecogame.data.remote.statistics.dto
 import com.dds.theecogame.domain.model.ranking.RankingUser
 
 data class RankingDto(
-    //val avatar: String,
+    val avatar: String?,
     val score: Int,
     val username: String
 )
 
 fun RankingDto.toRankingUser(): RankingUser {
     return RankingUser(
-        /*avatar, */score, username
+        avatar, score, username
     )
 }
