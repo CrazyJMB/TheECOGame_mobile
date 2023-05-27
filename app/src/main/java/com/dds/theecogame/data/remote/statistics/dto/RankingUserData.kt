073243c29dfe30cc0262a3961ee29c8b44f1d3ba
CrazyJMB <crazyjmbemail@gmail.com>
2023-05-27 17:28:04 +0200

    Fix - Fallos en la llamada de la API sobre currentUserRanking

--- app/src/main/java/com/dds/theecogame/data/remote/statistics/dto/RankingUserData.kt
+++ app/src/main/java/com/dds/theecogame/data/remote/statistics/dto/RankingUserData.kt
@@ -1,6 +1,12 @@
 package com.dds.theecogame.data.remote.statistics.dto
 
+import com.dds.theecogame.domain.model.ranking.CurrentUserRanking
+
 data class RankingUserData(
     val position: Int,
     val score: Int
 )
+
+fun RankingUserData.toCurrentUserRanking(): CurrentUserRanking {
+    return CurrentUserRanking(position, score)
+}
---
+++ app/src/main/java/com/dds/theecogame/domain/memento/Settings.kt
@@ -0,0 +1,5 @@
+package com.dds.theecogame.domain.memento
+
+data class Settings(
+    var generalVolume: Int
+)
