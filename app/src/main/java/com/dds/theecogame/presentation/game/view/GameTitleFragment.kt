83f7b023a73c8b15fb660f3381ed251a12fb642b
CrazyJMB <crazyjmbemail@gmail.com>
2023-05-29 21:48:05 +0200

    Cambios en el fragmento de titulo

--- app/src/main/java/com/dds/theecogame/presentation/game/view/GameTitleFragment.kt
+++ app/src/main/java/com/dds/theecogame/presentation/game/view/GameTitleFragment.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dds.theecogame.databinding.FragmentTitleQuestionBinding
+import com.dds.theecogame.databinding.FragmentTitleChallengesBinding
 
 class GameTitleFragment : Fragment() {
 
-    private lateinit var binding: FragmentTitleQuestionBinding
-    
+    private lateinit var binding: FragmentTitleChallengesBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentTitleQuestionBinding.inflate(inflater)
+        binding = FragmentTitleChallengesBinding.inflate(inflater)
         return binding.root
     }
 
